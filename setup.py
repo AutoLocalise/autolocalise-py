c050d32cabe74d24579c83b0ac9c29dadf977b29
wade <xxx@gmail.com>
2025-08-16 21:24:44 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="autolocalise",
-    version="0.1.0",
+    version="0.1.1",
     author="AutoLocalise",
     author_email="support@autolocalise.com",
     description="Python SDK for AutoLocalise translation service",
